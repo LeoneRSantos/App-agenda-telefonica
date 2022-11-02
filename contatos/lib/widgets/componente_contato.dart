8b9410668cc516930baba853ebdf4b6d8939d538
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-11-02 20:10:17 -0300

    Criação do widget que gera o componente de cada contato

---
+++ contatos/lib/widgets/componente_contato.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import '../models/contato.dart';
+
+class ComponenteContato extends StatelessWidget {
+  const ComponenteContato({Key? key, required this.contato}) : super(key: key);
+
+  final Contato contato;
+  @override
+  Widget build(BuildContext context) {
+    final avatarAlternativo =
+        contato.urlDoAvatar == null || contato.urlDoAvatar.isEmpty
+            ? CircleAvatar(
+                child: Icon(Icons.person),
+              )
+            : CircleAvatar(
+                backgroundImage: NetworkImage(contato.urlDoAvatar),
+              );
+    return ListTile( 
+      leading: avatarAlternativo,
+    );
+  }
+}
