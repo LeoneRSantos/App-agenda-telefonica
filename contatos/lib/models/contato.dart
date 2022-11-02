01d9da099869898aa091eb27f6529640163f887f
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-11-02 20:08:33 -0300

    Criação da classe Contato

---
+++ contatos/lib/models/contato.dart
@@ -0,0 +1,12 @@
+class Contato {
+  final String id;
+  final String nome;
+  final String numero;
+  final String urlDoAvatar;
+
+  Contato(
+      {required this.id,
+      required this.nome,
+      required this.numero,
+      required this.urlDoAvatar});
+}
