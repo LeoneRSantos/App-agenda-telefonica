7f0383dd27ebe4aa83ceb4a5ea5143fd84b828b9
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-11-03 10:16:28 -0300

    ComponenteContato: adição de condição para valor nulo no avatar

--- contatos/lib/widgets/componente_contato.dart
+++ contatos/lib/widgets/componente_contato.dart
@@ -7,15 +7,16 @@ class ComponenteContato extends StatelessWidget {
   final Contato contato;
   @override
   Widget build(BuildContext context) {
-    final avatarAlternativo =
-        contato.urlDoAvatar == null || contato.urlDoAvatar.isEmpty
-            ? CircleAvatar(
-                child: Icon(Icons.person),
-              )
-            : CircleAvatar(
-                backgroundImage: NetworkImage(contato.urlDoAvatar),
-              );
-    return ListTile( 
+    final avatarAlternativo = contato.urlDoAvatar == null ||
+            contato.urlDoAvatar.isEmpty ||
+            contato.urlDoAvatar == ''
+        ? CircleAvatar(
+            child: Icon(Icons.person),
+          )
+        : CircleAvatar(
+            backgroundImage: NetworkImage(contato.urlDoAvatar),
+          );
+    return ListTile(
       leading: avatarAlternativo,
     );
   }
