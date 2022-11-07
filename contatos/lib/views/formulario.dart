9c7dbb793b236917050272bb8b887cae384fe564
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-11-06 23:38:46 -0300

    Formulario: campos de input e botão para adicionar contato

--- contatos/lib/views/formulario.dart
+++ contatos/lib/views/formulario.dart
@@ -15,6 +15,54 @@ class Formulario extends StatelessWidget {
           ),
         ),
       ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 32.0, horizontal: 8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Nome',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Número',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'URL do avatar',
+                ),
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 12.0, vertical: 24.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  elevation: 0.0,
+                ),
+                onPressed: () { 
+                  Navigator.of(context).pop();
+                },
+                child: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text('Adicionar'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
