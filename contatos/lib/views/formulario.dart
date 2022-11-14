c97048a3380b888ca1960297bd79202a9aa4418c
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-11-14 09:40:37 -0300

    Formulario: adição de SingleChildScrollView e definição de cor da AppBar

--- contatos/lib/views/formulario.dart
+++ contatos/lib/views/formulario.dart
@@ -37,6 +37,7 @@ class _FormularioState extends State<Formulario> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
         elevation: 0.0,
         title: const Center(
           child: Text(
@@ -47,88 +48,90 @@ class _FormularioState extends State<Formulario> {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(vertical: 32.0, horizontal: 8.0),
-        child: Form(
-          key: _inputFormulario,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: TextFormField(
-                  initialValue: _auxiliarFormulario['nome'],
-                  decoration: const InputDecoration(
-                    labelText: 'Nome',
-                  ),
-                  validator: (valor) {
-                    if (valor == null || valor.trim().isEmpty) {
-                      return "Este campo está vazio.";
-                    } else {
-                      _inputFormulario.currentState!.save();
-                    }
-                  },
-                  onSaved: (valorInserido) =>
-                      _auxiliarFormulario['nome'] = valorInserido,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: TextFormField(
-                  initialValue: _auxiliarFormulario['numero'],
-                  decoration: const InputDecoration(
-                    labelText: 'Número',
-                  ),
-                  validator: (valor) {
-                    if (valor == null || valor.trim().isEmpty) {
-                      return "Este campo está vazio.";
-                    } else {
-                      _inputFormulario.currentState!.save();
-                    }
-                  },
-                  onSaved: ((valorInserido) =>
-                      _auxiliarFormulario['numero'] = valorInserido),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: TextFormField(
-                  initialValue: _auxiliarFormulario['urlDoAvatar'],
-                  decoration: const InputDecoration(
-                    labelText: 'URL do avatar',
-                  ),
-                  onSaved: (valorIserido) =>
-                      _auxiliarFormulario['urlDoAvatar'] = valorIserido,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 12.0, vertical: 24.0),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    elevation: 0.0,
-                  ),
-                  onPressed: () {
-                    final _valido = _inputFormulario.currentState?.validate();
-
-                    if (_valido!) {
-                      _inputFormulario.currentState?.save();
-
-                      Provider.of<ContatosProvider>(context, listen: false)
-                          .putContato(Contato(
-                        id: _auxiliarFormulario['id']!,
-                        nome: _auxiliarFormulario['nome']!,
-                        numero: _auxiliarFormulario['numero']!,
-                        urlDoAvatar: _auxiliarFormulario['urlDoAvatar'],
-                      ));
-                      Navigator.of(context).pop();
-                    }
-                  },
-                  child: const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Text('Adicionar'),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _inputFormulario,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: TextFormField(
+                    initialValue: _auxiliarFormulario['nome'],
+                    decoration: const InputDecoration(
+                      labelText: 'Nome',
+                    ),
+                    validator: (valor) {
+                      if (valor == null || valor.trim().isEmpty) {
+                        return "Este campo está vazio.";
+                      } else {
+                        _inputFormulario.currentState!.save();
+                      }
+                    },
+                    onSaved: (valorInserido) =>
+                        _auxiliarFormulario['nome'] = valorInserido,
                   ),
                 ),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: TextFormField(
+                    initialValue: _auxiliarFormulario['numero'],
+                    decoration: const InputDecoration(
+                      labelText: 'Número',
+                    ),
+                    validator: (valor) {
+                      if (valor == null || valor.trim().isEmpty) {
+                        return "Este campo está vazio.";
+                      } else {
+                        _inputFormulario.currentState!.save();
+                      }
+                    },
+                    onSaved: ((valorInserido) =>
+                        _auxiliarFormulario['numero'] = valorInserido),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: TextFormField(
+                    initialValue: _auxiliarFormulario['urlDoAvatar'],
+                    decoration: const InputDecoration(
+                      labelText: 'URL do avatar',
+                    ),
+                    onSaved: (valorIserido) =>
+                        _auxiliarFormulario['urlDoAvatar'] = valorIserido,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 12.0, vertical: 24.0),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      elevation: 0.0,
+                    ),
+                    onPressed: () {
+                      final _valido = _inputFormulario.currentState?.validate();
+        
+                      if (_valido!) {
+                        _inputFormulario.currentState?.save();
+        
+                        Provider.of<ContatosProvider>(context, listen: false)
+                            .putContato(Contato(
+                          id: _auxiliarFormulario['id'],
+                          nome: _auxiliarFormulario['nome']!,
+                          numero: _auxiliarFormulario['numero']!,
+                          urlDoAvatar: _auxiliarFormulario['urlDoAvatar'],
+                        ));
+                        Navigator.of(context).pop();
+                      }
+                    },
+                    child: const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text('Adicionar'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
